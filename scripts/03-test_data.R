b4a4f8ecfbaec65f681e3c378abf049839921c02
fazim07 <156146388+fazim07@users.noreply.github.com>
2024-01-23 12:38:08 -0500

    Update 03-test_data.R

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -17,7 +17,7 @@ library(tidyverse)
 data_clean_no_NA$year_of_death |> min() == 2017
 #Test for max/current year of data collection
 data_clean_no_NA$year_of_death |> max() == 2023
-#Test for the year of death to be numeric class
+#Test for the year of death to be a numeric class
 data_clean_no_NA$year_of_death |> class() == "numeric"
 #Test for death toll to be numeric class
 data_clean_no_NA$death_tolls |> class() == "numeric"
