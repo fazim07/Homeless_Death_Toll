1724eb1a15e1e14757d2d49ab94f4001bd392fc2
fazim07 <156146388+fazim07@users.noreply.github.com>
2024-01-24 09:07:46 -0500

    Typo fixed

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -21,10 +21,13 @@ set.seed(365)
 months <- c("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
  
 simulated_data <- tibble(
+ #loading for one specific month
   Month = rep(months, each = 1),
+ # for years 2017-2023 and for each month 
   Year_Death = rep(c(2017:2023), each = 12)
-  Death_Toll = runif(84, min=0, max=100)  
+ # run death toll 
+  Death_Toll = runif(84, min=0, max=200)  
 )
 
-# Display the simulated data
+#print simulated data 
 print(simulated_data)
